9d49ad0849c4a9f119a6fcea42b8c3367eca3b7e
Gustavo Rocha <gustavo@onserp.com.br>
2025-12-18 18:34:43 -0300

    add gunicorn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import instaloader
+import os
 
 app = Flask(__name__)
 
@@ -25,4 +26,5 @@ def error_message():
     return jsonify({'error': 'Invalid API endpoint. Use /get_profile_pic?username=<username>'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=False, port=80)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
